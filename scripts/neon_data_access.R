599eff5e67828e002d922bd85c003cb134f81c6f
Lindsey Stowell <linlinz413@gmail.com>
2026-02-17 11:36:26 -0500

    added import statements

---
+++ scripts/neon_data_access.R
@@ -0,0 +1,9 @@
+## Load packages & Raw Data Files
+
+install.packages("tidyverse")
+install.packages("here")
+install.packages("neonUtilities")
+
+library(tidyverse)
+library(here)
+library(neonUtilities)
