6b7f3b4cc9a7db913f1103d5a91d84c6ea52341f
Lindsey Stowell <linlinz413@gmail.com>
2026-02-19 10:12:11 -0500

    added data access file

--- scripts/neon_data_access.R
+++
@@ -1,9 +0,0 @@
-## Load packages & Raw Data Files
-
-install.packages("tidyverse")
-install.packages("here")
-install.packages("neonUtilities")
-
-library(tidyverse)
-library(here)
-library(neonUtilities)
